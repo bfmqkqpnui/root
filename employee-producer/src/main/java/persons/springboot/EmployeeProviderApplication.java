b747b6a34f4539aabc333db497bbd62d972c4b99
bfmqkqpnui <47877962@qq.com>
2018-07-06 15:23:33 +0800

    添加应用环境

--- employee-producer/src/main/java/persons/springboot/EmployeeProviderApplication.java
+++ employee-producer/src/main/java/persons/springboot/EmployeeProviderApplication.java
@@ -3,11 +3,12 @@ package persons.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Hello world!
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan(basePackages = {"persons.springboot.mapper"})
 public class EmployeeProviderApplication {
     public static void main(String[] args) {
